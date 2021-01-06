f1c7eea129a52ad027a126b1d9de7189a5022db8
ghfjkdls <61516241+ghfjkdls@users.noreply.github.com>
2021-01-06 19:13:10 +0100

    Add files via upload

---
+++ lista7/zad1lista7.c
@@ -0,0 +1,30 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <unistd.h>
+#include <string.h>
+#define NTHREADS	8
+
+void *Hello(void *threadid)
+{
+   printf("Hello SCR. Written by thread %ld\n", threadid);
+   pthread_exit(NULL);
+}
+
+int main()
+{
+pthread_t threads[NTHREADS];
+int rc;
+long t;
+for(t=0;t<NTHREADS;t++){
+  rc = pthread_create(&threads[t], NULL, Hello, (void *)t);
+  if (rc){
+    printf("ERROR: return code from pthread_create() is %d\n", rc);
+    printf("Code %d= %s\n",rc,strerror(rc));
+    exit(-1);
+    }
+   }
+//printf("Hello SCR. Written by thread %ld\n", threadid);
+pthread_exit(NULL);
+}
